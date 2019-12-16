9a4bbf83a241940a9b76a63e47e424374593f5e6
Yuri Yakovlev <hi@mynameisyuri.com>
2019-12-16 21:41:01 +0100

    Add LED strip functionality and refactor the code

--- smart-led/smart-led.ino
+++ smart-led/smart-led.ino
@@ -1,3 +1,4 @@
+#include <GyverRGB.h>
 #include <LiquidCrystal.h>
 
 #define BUTTON_PIN_MINUS 6
@@ -7,12 +8,41 @@
 // LCD settings
 #define LCD_COLUMN_NUMBER 16
 #define LCD_ROW_NUMBER 2
-#define LCD_RS 12 // Register Select
-#define LCD_E 11  // Enable
-#define LCD_DB4 5 // BD4
-#define LCD_DB5 4 // BD5
-#define LCD_DB6 3 // BD6
-#define LCD_DB7 2 // BD7
+#define LCD_RS_PIN 13 // Register Select
+#define LCD_E_PIN 12  // Enable
+#define LCD_DB4_PIN 5 // BD4
+#define LCD_DB5_PIN 4 // BD5
+#define LCD_DB6_PIN 3 // BD6
+#define LCD_DB7_PIN 2 // BD7
+
+// RGB LED settings
+#define LED_RED_PIN 9    // R
+#define LED_GREEN_PIN 10 // G
+#define LED_BLUE_PIN 11  // B
+#define LED_HUE_START 0  // Initial fire color
+#define LED_HUE_GAP 10
+#define LED_SMOOTH_K 0.15  // Fire smoothness coefficient
+#define LED_MIN_BRIGHT 80  // Min fire brightness
+#define LED_MAX_BRIGHT 255 // Max fire brightness
+#define LED_MIN_SAT 255    // Min saturation
+#define LED_MAX_SAT 255    // Max saturation
+
+int ledHueStart = LED_HUE_START;
+
+class ColorMode {
+private:
+  int _id;
+  String _name;
+  int _value;
+
+public:
+  ColorMode(int id, String name, int value)
+      : _id(id), _name(name), _value(value) {}
+
+  int getId() { return _id; }
+  String getName() { return _name; }
+  int getValue() { return _value; }
+};
 
 class Button {
 private:
@@ -60,17 +90,49 @@ public:
 
 Button minusButton(BUTTON_PIN_MINUS);
 Button plusButton(BUTTON_PIN_PLUS);
-String colorModes[COLORS_COUNT] = {"Red", "Green", "Blue", "Purple",
-                                   "Ocean wave"};
+ColorMode colorModes[] = {ColorMode(0, "Red", 0), ColorMode(1, "Green", 80),
+                          ColorMode(2, "Blue", 140),
+                          ColorMode(3, "Purple", 190),
+                          ColorMode(4, "Ocean wave", 240)};
 int count = 0;
 
-LiquidCrystal lcd(LCD_RS, LCD_E, LCD_DB4, LCD_DB5, LCD_DB6, LCD_DB7);
+LiquidCrystal lcd(LCD_RS_PIN, LCD_E_PIN, LCD_DB4_PIN, LCD_DB5_PIN, LCD_DB6_PIN,
+                  LCD_DB7_PIN);
+GRGB strip(LED_RED_PIN, LED_GREEN_PIN, LED_BLUE_PIN);
 
 void clearLCDLine(int lineNumber) {
   lcd.setCursor(0, lineNumber);
 
   for (int n = 0; n < LCD_COLUMN_NUMBER; n++) {
     lcd.print(" ");
+  }
+}
+
+void fireTick() {
+  static uint32_t prevTime, prevTime2;
+  static byte fireRnd = 0;
+  static float fireValue = 0;
+
+  // Set fire direction
+  if (millis() - prevTime > 100) {
+    prevTime = millis();
+    fireRnd = random(0, 10);
+  }
+
+  // Move the flame
+  if (millis() - prevTime2 > 20) {
+    prevTime2 = millis();
+    fireValue = (float)fireValue * (1 - LED_SMOOTH_K) +
+                (float)fireRnd * 10 * LED_SMOOTH_K;
+    strip.setHSV(
+        constrain(
+            map(fireValue, 20, 60, ledHueStart, ledHueStart + LED_HUE_GAP), 0,
+            255), // H
+        constrain(map(fireValue, 20, 60, LED_MAX_SAT, LED_MIN_SAT), 0,
+                  255), // S
+        constrain(map(fireValue, 20, 60, LED_MIN_BRIGHT, LED_MAX_BRIGHT), 0,
+                  255) // V
+    );
   }
 }
 
@@ -84,27 +146,35 @@ void setup() {
   // Initialize the LCD
   lcd.begin(LCD_COLUMN_NUMBER, LCD_ROW_NUMBER);
   lcd.print("Choose the mode:");
+
+  // Initialize the LED
+  strip.setBrightness(255);
+  strip.setDirection(REVERSE);
 }
 
 void loop() {
+  fireTick();
+
   lcd.setCursor(0, 1);
-  lcd.print(colorModes[count]);
+  lcd.print(colorModes[count].getName());
+
+  Serial.println(colorModes[count].getId());
+  Serial.println(colorModes[count].getName());
+  Serial.println(colorModes[count].getValue());
 
   if (minusButton.isPressed()) {
     Serial.println(F("Pressed"));
   }
 
-  if (minusButton.isReleased()) {
-    if (count > 0) {
-      clearLCDLine(1);
-      count--;
-    }
+  if (minusButton.isReleased() && count > 0) {
+    clearLCDLine(1);
+    count--;
+    ledHueStart = colorModes[count].getValue();
   }
 
-  if (plusButton.isReleased()) {
-    if (count < COLORS_COUNT - 1) {
-      clearLCDLine(1);
-      count++;
-    }
+  if (plusButton.isReleased() && count < COLORS_COUNT - 1) {
+    clearLCDLine(1);
+    count++;
+    ledHueStart = colorModes[count].getValue();
   }
 }
